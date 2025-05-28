e10e8edc2791dfe9e78c8a38989f513bb54c3c60
Fidella1 <109987595+Fidella1@users.noreply.github.com>
2025-05-28 11:27:47 +0200

    Delete Genre.js

--- Genre.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const genreSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-});
-
-const Genre = mongoose.model('Genre', genreSchema);
-export default Genre;
